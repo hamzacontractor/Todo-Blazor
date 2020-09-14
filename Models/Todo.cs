f351598f638e784028ed901c0a879a4f976ce136
Hamza Contractor <hamza.contractor@gmail.com>
2020-09-14 09:45:31 +0530

    Initial Commit

---
+++ Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo_Blazor_App.Models {
+   public class Todo {
+      public string Text { get; set; }
+      public DateTime TimeAdded { get; set; }
+
+      public string Priority { get; set; }
+
+
+   }
+
+
+}
